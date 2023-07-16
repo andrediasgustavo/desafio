3240d57ebbfb0298dd889a82763b2311f3e6b623
André Dias <andrediasgustavo@gmail.com>
2023-07-16 14:17:33 -0300

    Add appicons

---
+++ desafio/Home/View/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  desafio
+//
+//  Created by Andre Dias on 16/07/23.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
